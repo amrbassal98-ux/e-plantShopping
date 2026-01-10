90ca24a1abf42341516232221afa2ad68a87a415
Amr Bassal <amrbassal98@gmail.com>
2026-01-10 08:23:19 +0200

    created cartSlice logic

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -3,17 +3,30 @@ import { createSlice } from '@reduxjs/toolkit';
 export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
-    items: [], // Initialize items as an empty array
+    items: [],
   },
   reducers: {
     addItem: (state, action) => {
+      const product = action.payload;
+      const existingProduct = state.items.find((item) => product.name === item.name);
+      if (!existingProduct) {
+        product.quantity = 1;
+        state.items.push(product);
+      } else {
+        product.quantity ++;
+      };
     
     },
     removeItem: (state, action) => {
+      const product = action.payload;
+      state.items = state.items.filter((item) => product.name !== item.name);
     },
     updateQuantity: (state, action) => {
-
-    
+      const product = action.payload;
+      const productToUpdate = state.items.find((item) => item.name === product.name);
+      if (productToUpdate) {
+      productToUpdate.quantity = product.quantity;
+      }
     },
   },
 });
